e1bbc104261f09a6c3d167d9116e5f5dba1348ed
강기훈 <rlgns0374@gmail.com>
2025-08-04 08:58:01 +0900

    js_실습

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+document.write("<h1>hello skala</h1>")
+document.write("<h2>hello skala</h2>")
+document.write("<h3>hello skala</h3>")
+document.write("<h4>hello skala</h4>")
+document.write("<h5>hello skala</h5>")
+document.write("<h6>hello skala</h6>")
+
+
+
